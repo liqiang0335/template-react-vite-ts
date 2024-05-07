3af2ed0465ce130148711d00381816662ab8a2a7
liqiang <liqiang0335@qq.com>
2024-05-07 14:58:39 +0800

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { AppWrapper } from "./AppWrapper";
 import { RoutePages } from "./routes";
+import { ErrorBoundary } from "react-error-boundary";
 
-function App() {
+export function App() {
   return (
     <AppWrapper>
       <div>
         <div className="y-border-b text-center p-2">Header</div>
         <div className="bg-white p-2 min-h-[300px]">
-          <RoutePages></RoutePages>
+          <ErrorBoundary key={window.location.hash} fallback={<div>出错了</div>}>
+            <RoutePages></RoutePages>
+          </ErrorBoundary>
         </div>
         <div className="y-border-t text-center p-2">Footer</div>
       </div>
     </AppWrapper>
   );
 }
-
-export default App;
--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -4,18 +4,13 @@ import { HashRouter } from "react-router-dom";
 import { queryClient } from "./components/queryClient";
 import { AntConfig } from "./components/common/AntConfig";
 import { Toaster } from "@/components/ui/sonner";
-import { ErrorBoundary } from "react-error-boundary";
 
 export function AppWrapper({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <AntConfig>
-        <HashRouter>
-          <ErrorBoundary key={window.location.hash} fallback={<div>出错了</div>}>
-            {children}
-          </ErrorBoundary>
-          <Toaster position="top-center" duration={1500} />
-        </HashRouter>
+        <HashRouter>{children}</HashRouter>
+        <Toaster position="top-center" duration={1500} />
       </AntConfig>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
